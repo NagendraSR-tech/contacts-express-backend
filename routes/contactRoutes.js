b20030b2c94cde2d2a10a68128c2859dab195843
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-28 10:21:49 +0530

    setup Route for contacts CRUD

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,20 @@ router.route("/").get((req, res) => {
     res.status(200).json({ message: "Get all contacts" });
 });
 
+router.route("/").post((req, res) => {
+    res.status(201).json({ message: "Create a new contact" });
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json({ message: `Get a contact with ID: ${req.params.id}` });
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).json({ message: `Update a contact with ID: ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).json({ message: `Delete a contact with ID: ${req.params.id}` });
+});
+
 module.exports = router;
