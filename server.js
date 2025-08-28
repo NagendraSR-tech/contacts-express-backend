a2a8c67e3f5272aff343e9f76f771ec689744607
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-28 09:33:27 +0530

    add Express Router

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-    // res.send("Welcome to the Contacts API");
-    res.status(200).json({ message: "Get all contacts" });
-});
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     //console.log(`Server is running on http://localhost:${port}`)
