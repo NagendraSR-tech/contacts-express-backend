8e08d610950ed408d68c2592a82a93a9bd551f41
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-29 20:03:35 +0530

    add user Routes - Registration, Login and Current

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+    res.json({ message: "Register a user" });
+});
+router.post("/login", (req, res) => {
+    res.json({ message: "Login user" });
+});
+
+router.get("/current", (req, res) => {
+    res.json({ message: "Current user information" });
+});
+
+module.exports = router;
