56e1a3c13ae6aec021cf75fab70d8288d8474909
Nagendra Kumar S R <nagendrasr.webdesigner@gmail.com>
2025-08-29 22:45:13 +0530

    user Registration and Password Hashing, install bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,45 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
 
 //@desc    Register a user
 //@route   POST /api/users/register
 //@access  Public
 const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("Please provide all required fields");
+    }
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password:", hashedPassword);
+    // Create user
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+    console.log(`user created: ${user}`);
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+        });
+    }
+    else {
+        res.status(400);
+        throw new Error("Invalid user data"); //request not valid - not able to create new user
+    }
+
+    // Temporary response for testing
     res.json({ message: "Register a user" });
 });
 
